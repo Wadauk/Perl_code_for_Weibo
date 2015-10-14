14cc1d992e7148304c7c11cd08a19407d01f4623
Wadauk <2336221276@qq.com>
2015-10-14 14:35:02 +0800

    Create new file

---
+++ No.1/code3.pl
@@ -0,0 +1,17 @@
+#! /usr/bin/perl -ws
+my $filename = $ARGV[0];
+open (OUT, ">$filename")||die "open error!\n";
+$random_sequences = &random_sequences_productor($ARGV[1]);
+print OUT "$random_sequences\n";
+
+sub random_sequences_productor{
+	my $sequence_length = $_[0];
+	my $sequence_random;
+	for (my $i = 0;$i < $sequence_length;$i++){
+		$sequence_random .= int(rand(4)+1);
+	}
+	$sequence_random=~tr/1234/ATGC/;
+	$sequence_random;
+}
+
+code by @每天学点Perl语言
